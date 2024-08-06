43809d9ec1b7c9329d817254b25899bc74224705
Shahista <shikalgarshahista17@gmial.com>
2024-08-06 14:38:43 +0530

    this is my firstcommit

---
+++ src/main/java/com/application/SpringCruddemo/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.application.SpringCruddemo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.application.SpringCruddemo.domain.Student;
+
+
+@Repository
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
---
+++ src/test/java/com/application/SpringCruddemo/SpringCruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.SpringCruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
